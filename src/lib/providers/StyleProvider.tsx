cd1c790cd56fb79e6f7190c19866a465240330bf
Dominick Bioc <dbioc917@gmail.com>
2025-09-25 01:07:20 -0500

    - user has style persistence

--- src/lib/providers/StyleProvider.tsx
+++ src/lib/providers/StyleProvider.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, use, useCallback, useContext, useEffect, useMemo, useState} from "react";
+import React, {createContext, useCallback, useContext, useEffect, useMemo, useState} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import type { Style } from '@/types/common'
 import { useAuth } from '@/lib/AuthProvider'
